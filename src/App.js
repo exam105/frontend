b7491cb8916335bfc7bfbd092c1af317c85000ce
Haseeb <haseebbajwa1@outlook.com>
2021-06-07 20:48:56 +0500

    added suspense

--- src/App.js
+++ src/App.js
@@ -1,33 +1,53 @@
-import React from "react";
-import "./App.css";
+import React, { Suspense } from "react";
 import { Route, Switch } from "react-router-dom";
 import AdminIndex from "./AdminPanel/Pages/AdminIndex";
-import AdminAddmcqs from "./AdminPanel/Pages/AdminAddmcqs";
-import AdminAddBoard from "./AdminPanel/Pages/AdminAddBoard";
-import AdminChoiceAndTheory from "./AdminPanel/Pages/AdminChoiceAndTheory";
 import AdminPapers from "./AdminPanel/Pages/AdminPapers";
-import AdminSearch from "./AdminPanel/Pages/AdminSearch";
-import AdminAddTheory from "./AdminPanel/Pages/AdminAddTheory.js";
-import AddDataOperator from "./AdminPanel/Pages/AddDataOperator";
-
-// Importing Bootstrap
+// Bootstrap and Styles
 import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+// Components
+const AdminAddmcqs = React.lazy(() =>
+  import("./AdminPanel/Pages/AdminAddmcqs")
+);
+const AdminAddBoard = React.lazy(() =>
+  import("./AdminPanel/Pages/AdminAddBoard")
+);
+const AdminChoiceAndTheory = React.lazy(() =>
+  import("./AdminPanel/Pages/AdminChoiceAndTheory")
+);
+const AdminSearch = React.lazy(() => import("./AdminPanel/Pages/AdminSearch"));
+const AdminAddTheory = React.lazy(() =>
+  import("./AdminPanel/Pages/AdminAddTheory.js")
+);
+const AddDataOperator = React.lazy(() =>
+  import("./AdminPanel/Pages/AddDataOperator")
+);
 
 function App() {
   return (
     <Switch>
-      <Route exact path="/admin/panel/" component={AdminIndex} />
-      <Route exact path="/admin/panel/papers" component={AdminPapers} />
-      <Route exact path="/admin/panel/search" component={AdminSearch} />
-      <Route exact path="/admin/panel/add/" component={AdminChoiceAndTheory} />
-      <Route exact path="/admin/panel/add/mcqs" component={AdminAddmcqs} />
-      <Route exact path="/admin/panel/add/papers" component={AdminAddBoard} />
-      <Route exact path="/admin/panel/add/theory" component={AdminAddTheory} />
-      <Route
-        exact
-        path="/d/a/t/a/e/n/t/r/y/o/p/e/r/a/t/o/r/"
-        component={AddDataOperator}
-      />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Route exact path="/admin/panel/" component={AdminIndex} />
+        <Route exact path="/admin/panel/papers" component={AdminPapers} />
+        <Route exact path="/admin/panel/search" component={AdminSearch} />
+        <Route
+          exact
+          path="/admin/panel/add/"
+          component={AdminChoiceAndTheory}
+        />
+        <Route exact path="/admin/panel/add/mcqs" component={AdminAddmcqs} />
+        <Route exact path="/admin/panel/add/papers" component={AdminAddBoard} />
+        <Route
+          exact
+          path="/admin/panel/add/theory"
+          component={AdminAddTheory}
+        />
+        <Route
+          exact
+          path="/d/a/t/a/e/n/t/r/y/o/p/e/r/a/t/o/r/"
+          component={AddDataOperator}
+        />
+      </Suspense>
     </Switch>
   );
 }
