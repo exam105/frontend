8e30336f31b494bd2288bed893396817580813b6
Haseeb <haseebbajwa1@outlook.com>
2021-06-11 15:19:37 +0500

    improved code splitting

--- src/AdminPanel/Components/AdminPapersComponent.js
+++ src/AdminPanel/Components/AdminPapersComponent.js
@@ -6,7 +6,6 @@ import { makeStyles } from "@material-ui/core/styles";
 import {
   Table,
   TableBody,
-  TableCell,
   TableContainer,
   TableHead,
   TableRow,
@@ -15,6 +14,7 @@ import {
   Checkbox,
 } from "./LazyImports/MaterialUI";
 import Paper from "@material-ui/core/Paper";
+import TableCell from "@material-ui/core/TableCell";
 // Components
 import { EnhancedTableToolbar } from "./LazyImports/LocalComponents";
 
